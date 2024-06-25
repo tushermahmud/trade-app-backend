ca365f52ea7800f2e6b8817c3bfcb1a4dc717d1e
tushermahmud <=>
2024-06-25 18:46:49 +0600

    connected socket

---
+++ helper.js/utils.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const getTradeData = async () => {
+    try {
+         try {
+           const masterTradeResponse = await axios.get(process.env.LAMBDA_URL);
+           const masterTrade = masterTradeResponse.data;
+           const mt4LoginResponse = await axios.get(
+             `${process.env.MT4_API}/Connect?user=${process.env.MT4_API_USER}&password=${process.env.MT4_API_PASSWORD}&host=${process.env.MT4_API_HOST}&port=${process.env.MT4_API_PORT}`
+           );
+           const connectionId = mt4LoginResponse.data;
+           const tradeResponse = await axios.get(
+             `${process.env.MT4_API}/OrderSend`,
+             {
+               params: {
+                 id: connectionId,
+                 symbol: masterTrade.symbol,
+                 operation: masterTrade.operation,
+                 volume: masterTrade.volume,
+                 takeprofit: masterTrade.takeprofit,
+                 comment: "Blackalgo #",
+               },
+             }
+           );
+           return tradeResponse.data;
+         } catch (error) {
+           console.log("Error processing trade:", error.message);
+         }
+    } catch (error) {
+        console.log("Error processing trade:", error.message);
+    }
+}
+
+module.exports = { getTradeData };
---
+++ index.js
@@ -0,0 +1,33 @@
+// server.js
+const express = require("express");
+const WebSocket = require("ws");
+const axios = require("axios");
+const dotenv = require("dotenv");
+const http = require("http");
+const { getTradeData } = require("./helper.js/utils");
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("WebSocket server with Express");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+const wss = new WebSocket.Server({ server });
+wss.on('connection', function connection(ws) {
+  ws.on('message', async function incoming(message) {
+    const messageString = message.toString();
+    if (messageString === "Trade") {
+     const tradeResponse = await getTradeData();
+     ws.send(JSON.stringify(tradeResponse));
+    }
+  });
+});
+
